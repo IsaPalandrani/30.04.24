5343f3f264734303e8aa40227f9ec1bf625f44c2
IsaPalandrani <143020084+IsaPalandrani@users.noreply.github.com>
2024-04-30 11:51:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+class Calebe{
+    constructor (site){
+        this.site = site;
+    }
+
+    exibirSite (){
+        window.open(this.site);
+    }
+}
+
+var novoCalebe = new Calebe ("https://www.tiktok.com/@endrick/video/7355302397683698950?lang=pt-BR");
+novoCalebe.exibirSite();
